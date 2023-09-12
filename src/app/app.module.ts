5dca9657a2a98527805c7dfad37fc80b06c23916
HapaxPablo <pahapasha11@gmail.com>
2023-09-12 15:51:25 +0700

    wip: добавление g-zorro material UI, решение ошибки tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LeafletDrawModule } from '@asymmetrik/ngx-leaflet-draw';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, LeafletModule, HttpClientModule, LeafletDrawModule],
+  declarations: [
+    AppComponent,
+    SideBarComponent
+  ],
+  imports: [BrowserModule, LeafletModule, HttpClientModule, LeafletDrawModule, BrowserAnimationsModule, MatButtonModule, MatDialogModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
