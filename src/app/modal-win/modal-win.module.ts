466439d0b93651d4e9899567391366315d63dda9
HapaxPablo <pahapasha11@gmail.com>
2023-09-15 17:47:42 +0700

    refactor: модули ng-zorro и формы

--- src/app/modal-win/modal-win.module.ts
+++ src/app/modal-win/modal-win.module.ts
@@ -4,11 +4,14 @@ import * as AllIcons from '@ant-design/icons-angular/icons';
 import { IconDefinition } from '@ant-design/icons-angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { NzModalModule } from 'ng-zorro-antd/modal';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N, ru_RU } from 'ng-zorro-antd/i18n';
 import { NZ_ICONS } from 'ng-zorro-antd/icon';
 import { NzRateModule } from 'ng-zorro-antd/rate';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import {NzFormModule} from "ng-zorro-antd/form";
 
 const antDesignIcons = AllIcons as {
   [key: string]: IconDefinition;
@@ -19,7 +22,7 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
 
 @NgModule({
   declarations: [ModalWinComponent],
-  imports: [BrowserModule, NzModalModule, FormsModule, BrowserAnimationsModule, NzRateModule],
+  imports: [BrowserModule, NzModalModule, FormsModule, BrowserAnimationsModule, NzRateModule, NzInputModule, NzCheckboxModule, ReactiveFormsModule, NzFormModule],
   providers: [
     { provide: NZ_I18N, useValue: ru_RU },
     { provide: NZ_ICONS, useValue: icons },
